f236351c9b839e72529e30f33f87d3939b660bd7
kim <kim@museit.co.jp>
2020-05-01 19:13:17 +0900

    [modify]userによるデータ取得コード修正

--- my_english_app/my_english_app/ViewController/LearnViewController.swift
+++ my_english_app/my_english_app/ViewController/LearnViewController.swift
@@ -17,64 +17,50 @@ import FirebaseFirestore
 class LearnViewController: UIViewController {
     
     @IBOutlet weak var japanese: UILabel!
-    
     @IBOutlet weak var englishWord: UILabel!
-    
-   
     /*override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated:true)
-        
-        
     }*/
-    
-    
     override func viewDidLoad() {
-        
-        
         
         super.viewDidLoad()
         
-        
         let db = Firestore.firestore()
-        db.collection("words").whereField("completefg", isEqualTo: 0).getDocuments{(Snapshot,err) in
+        let userID = Auth.auth().currentUser?.uid
+        
+        if let user = userID{
+            print("userID:" + user)
             
+        }
+            
+        
+        db.collection("words").whereField("userId", isEqualTo: userID).getDocuments{(Snapshot,err) in
             
             if err != nil {
-                
                 print(err)
-                
-            }
+                }
             
-                for document in (Snapshot?.documents)!{
-                    
-                    if let readfg = document.data()["readfg"] as? Int {
-                        
+            for document in (Snapshot?.documents)!{
+                
+                print(document.data())
+                
+                
+                //let data = document.data()
+                
+                /*if let readfg = document.data()["newWords"] as? Int {
                         switch readfg {
                         case 0:
                            if let english = document.data()["english"] as? String{
-                                 
                                  self.englishWord.text = english
                              }
                              if let japanese = document.data()["japanese"] as? String{
-                                 
                                  self.japanese.text = japanese
                             }
-                            
-                           
-                        default:
+                            default:
                             break
                             }
-                        }
-                        
+                        }*/
                     }
-            
                 }
             }
         }
-        
-    
-    
-
-
-
-
